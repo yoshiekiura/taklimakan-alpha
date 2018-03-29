f6532a10e12a32d7d99ccdbbfb19ee0dfd64232f
Gotz <sgotsulyak@usetech.ru>
2018-03-29 11:50:55 +0300

    Security and Roles

--- src/Controller/ChartsController.php
+++ src/Controller/ChartsController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 //use Symfony\Component\Form\AbstractType;
 //use Symfony\Component\Form\FormBuilderInterface;
 //use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -32,4 +35,30 @@ class ChartsController extends Controller
             'charts' => $charts,
         ]);
     }
+
+    /**
+     * @Route("/api/charts/{type}", name="api_charts")
+     */
+    public function showChart($type, Request $request)
+    {
+
+//        $ChartsRepo = $this->getDoctrine()->getRepository(Charts::class);
+//        $Charts = $ChartsRepo->findAll();
+//var_dump($Charts);
+//die();
+        // $greeting = $generator->getRandomGreeting();
+        // $logger->info("Saying $greeting to $name!");
+
+        //        $logger->info("Saying hello to $name!");
+        //		return new Response("Hello $name!");
+
+var_dump($type);
+die();
+
+        return $this->render('charts/index.html.twig', [
+            //'controller_name' => 'ChartsController',
+            'charts' => $charts,
+        ]);
+    }
+
 }
