cbd7c8534db571e222409d111a5e8b6dca3f1b68
tavvet <anton.rudakov@protonmail.com>
2018-05-10 12:18:44 +0700

    investor profile base template

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use function PHPSTORM_META\type;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/profile")
@@ -13,15 +14,27 @@ class ProfileController extends Controller
 {
     /**
      * @Route("")
+     * @Method(methods={"GET"})
      */
     public function viewAction()
     {
         return $this->render('profile/view.html.twig', [
             'hide_footer' => true,
-            'news_list' => $this->getDoctrine()->getRepository('App:News')->getLastNews(3),
+            'news_list' => $this->getDoctrine()->getRepository('App:News')->getNews([
+                'limit' => 4,
+            ]),
             'courses' => $this->getDoctrine()->getRepository('App:Course')->getCourses([
                 'limit' => 4,
             ]),
         ]);
+    }
+
+    /**
+     * @Route("/edit")
+     * @Route(methods={"GET", "POST"})
+     */
+    public function editAction(Request $request)
+    {
+        return $this->render('profile/edit.html.twig');
     }
 }
