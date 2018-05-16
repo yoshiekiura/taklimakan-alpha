03013f70a2fca4f570f999b2d7c50dc220c58d64
j8kin <emk161278@gmail.com>
2018-05-16 18:01:33 +0300

    Scroll too the element first and only then click on it

--- tests/Selenium/IntegrationTests/features/main_page.feature
+++ tests/Selenium/IntegrationTests/features/main_page.feature
@@ -8,22 +8,22 @@ Feature: Main page
     When I click See all news on the main page
     Then I should see News index page
 
-  Scenario: Open news list from the main page by top menu button
-    When I click News button in top menu
-    Then I should see News index page
+ # Scenario: Open news list from the main page by top menu button
+ #   When I click News button in top menu
+ #   Then I should see News index page
 
-  Scenario: Open courses list from the main page by See all button
-    When I click See all Courses and Articles on the main page
-    Then I should see Courses index page
+  #Scenario: Open courses list from the main page by See all button
+  #  When I click See all Courses and Articles on the main page
+  #  Then I should see Courses index page
 
-  Scenario: Open courses list from the main page by top menu button
-    When I click Education button in top menu
-    Then I should see Courses index page
+  #Scenario: Open courses list from the main page by top menu button
+  #  When I click Education button in top menu
+  #  Then I should see Courses index page
 
-  Scenario: Open Analytics from banner
+  #Scenario: Open Analytics from banner
 
-  Scenario: Open Analytics from top menu
+  #Scenario: Open Analytics from top menu
 
-  Scenario: Subscribe to news
+  #Scenario: Subscribe to news
 
-  Scenario: Check footer links
+  #Scenario: Check footer links
--- tests/Selenium/IntegrationTests/features/steps/steps.py
+++ tests/Selenium/IntegrationTests/features/steps/steps.py
@@ -1,30 +1,45 @@
-from behave import *
-from selenium.webdriver.common.by import By
 import time
+
+from behave import *
+
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 use_step_matcher("re")
 
+"""
 ###GIVEN###
+"""
+
 @given("Taklimakan Network is opened and start popup is skipped")
 def step_impl(context):
     context.browser.find_element(By.CSS_SELECTOR, "button.btn.btn-buy").click()
     time.sleep(3)
 
+
+"""
 ###WHEN###
-#TODO this step is not working
+"""
 @when('I click See all news on the main page')
 def step_impl(context):
-    context.browser.find_element(By.LINK_TEXT, 'See all news').click()
+    # TODO this step is not working
+    # context.browser.find_element(By.LINK_TEXT, 'See all news').click()
+    all_news_link: WebElement = context.browser.find_element_by_link_text('See all news')
+    print(all_news_link)
+    context.browser.execute_script("arguments[0].scrollIntoView();", all_news_link)
+    all_news_link.click()
 
 @when("I click News button in top menu")
 def step_impl(context):
     context.browser.find_element(By.LINK_TEXT, 'News').click()
 
+
 ###THEN###
 @then("I should see News index page")
 def step_impl(context):
     EC.title_contains('Taklimakan / News')
+    # assert context.browser.title == 'Taklimakan / News'
 
 
 @when("I click See all Courses and Articles on the main page")
