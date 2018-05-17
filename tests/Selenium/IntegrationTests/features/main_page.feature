c593db88bc8f16f59c9274ecd1adaae791158840
smartdrop85 <kalmik.sasa@gmail.com>
2018-05-17 13:46:48 +0300

    main page automated

--- tests/Selenium/IntegrationTests/features/main_page.feature
+++ tests/Selenium/IntegrationTests/features/main_page.feature
@@ -5,7 +5,7 @@ Feature: Main page
     Given Taklimakan Network is opened and start popup is skipped
 
   Scenario: Open news list from the main page by See all button
-    When I click See all news on the main page
+    When I click 'See all news' on the main page
     Then I should see 'News' index page
 
   Scenario: Open news list from the main page by top menu button
@@ -13,7 +13,7 @@ Feature: Main page
     Then I should see 'Taklimakan News' index page
 
   Scenario: Open courses list from the main page by See all button
-    When I click See all Courses and Articles on the main page
+    When I click 'See all Courses and Articles' on the main page
     Then I should see 'Taklimakan Courses' index page
 
   Scenario: Open courses list from the main page by top menu button
@@ -21,9 +21,14 @@ Feature: Main page
     Then I should see 'Taklimakan Courses' index page
 
   Scenario: Open Analytics from banner
+    When I click 'View Analytics' on the main page
+    Then I should see Crypto100 chart
 
   Scenario: Open Analytics from top menu
+    When I click 'Analytics' button in top menu
+    Then I should see Crypto100 chart
 
   Scenario: Subscribe to news
-
-  Scenario: Check footer links
+    When I input my email into subscription form
+    And I hit Subscribe button
+    Then I should see You have been subscribed message
--- tests/Selenium/IntegrationTests/features/steps/steps.py
+++ tests/Selenium/IntegrationTests/features/steps/steps.py
@@ -5,6 +5,7 @@ from behave import *
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 use_step_matcher("parse")
 
@@ -14,38 +15,47 @@ use_step_matcher("parse")
 
 @given("Taklimakan Network is opened and start popup is skipped")
 def step_impl(context):
-    context.browser.find_element(By.CSS_SELECTOR, "button.btn.btn-buy").click()
-    time.sleep(3)
-
-
+    if len(context.browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-buy")) == 1:
+        context.browser.find_element(By.CSS_SELECTOR, "button.btn.btn-buy").click()
+        time.sleep(1)
+    else:
+        pass
 """
 ###WHEN###
 """
-@when('I click See all news on the main page')
-def step_impl(context):
-    # TODO this step is not working
-    # context.browser.find_element(By.LINK_TEXT, 'See all news').click()
-    all_news_link: WebElement = context.browser.find_element_by_link_text('See all news')
-    print(all_news_link)
-    context.browser.execute_script("arguments[0].scrollIntoView();", all_news_link)
-    all_news_link.click()
+@when(u"I click '{link}' on the main page")
+def step_impl(context,link):
+    all_link: WebElement = context.browser.find_element(By.LINK_TEXT, link)
+    context.browser.execute_script("arguments[0].scrollIntoView();", all_link)
+    all_link.click()
 
 @when(u"I click '{text}' button in top menu")
 def step_impl(context, text):
     context.browser.find_element(By.LINK_TEXT, text).click()
 
+@when("I input my email into subscription form")
+def step_impl(context):
+    form = context.browser.find_element(By.ID, 'exampleInputEmail1')
+    form.clear()
+    form.send_keys('test@yopmail.com')
 
+@when("I hit Subscribe button")
+def step_impl(context):
+    context.browser.find_element(By.CSS_SELECTOR, 'a.btn-sub').click()
+
+"""
 ###THEN###
-#TODO implement correct assertions
+"""
 @then(u"I should see '{text}' index page")
 def step_impl(context, text):
     print(context.browser.title)
-    context.assertEqual(context.browser.title, text)
+    assert text in context.browser.title
 
-
-@when("I click See all Courses and Articles on the main page")
+@then("I should see Crypto100 chart")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
+    context.browser.find_element(By.CSS_SELECTOR, 'div#crypto-index-card')
+
+#TODO implement step when subscription in implemented in TKLN
+@then("I should see You have been subscribed message")
+def step_impl(contect):
     pass
