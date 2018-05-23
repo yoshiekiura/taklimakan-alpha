7bf638d96beaab936eeb49f1251ea5458641a689
Gotz <sgotsulyak@usetech.ru>
2018-05-23 18:01:11 +0300

    Experiments with Reverse Proxies like Varnish

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Kernel;
+
+// Will be using Nginx Proxy & FastCGI Cache instead of Symfony Proxy
+// use App\CacheKernel;
+
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,6 +37,10 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 }
 
 $kernel = new Kernel($env, $debug);
+
+// Wrap the default Kernel with the CacheKernel one
+// $kernel = new CacheKernel($kernel);
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
---
+++ src/CacheKernel.php
@@ -0,0 +1,11 @@
+<?php
+
+// https://symfony.com/doc/current/http_cache.html
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+class CacheKernel extends HttpCache
+{
+}
