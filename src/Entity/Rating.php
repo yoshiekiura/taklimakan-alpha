91051646823f614e44b6e2d2495807440dabd061
Gotz <sgotsulyak@usetech.ru>
2018-05-27 21:19:34 +0300

    Start working on Ratings

---
+++ src/Entity/Rating.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Doctrine\ORM\Mapping\Index;
+use Doctrine\ORM\Mapping\UniqueConstraint;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RatingRepository")
+ * @ORM\Table(name="ratings", options={"charset"="utf8mb4", "collate"="utf8mb4_unicode_ci"}, uniqueConstraints={@UniqueConstraint(name="unique_like", columns={"content_type", "content_id", "user_id"})})
+ */
+class Rating
+{
+
+    public function __construct() {
+    }
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="integer", nullable=false, options={"unsigned": true})
+     */
+    private $user_id;
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+
+    /**
+     * @ORM\Column(type="string", length=32, nullable=true)
+     */
+    private $content_type;
+    public function getType()
+    {
+        return $this->content_type;
+    }
+    public function setType($type)
+    {
+        $this->content_type = $type;
+    }
+
+    /**
+     * @ORM\Column(type="integer", nullable=false, options={"unsigned": true})
+     */
+    private $content_id;
+    public function getContentId()
+    {
+        return $this->content_id;
+    }
+    public function setContentId($content_id)
+    {
+        $this->content_id = $content_id;
+    }
+
+
+    /**
+     * @ORM\Column(type="integer", nullable=true, options={"unsigned": true})
+     */
+    private $rating;
+    public function getRating()
+    {
+        return $this->rating;
+    }
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+    }
+
+
+    /**
+     * @ORM\Column(type="datetime", options={"default"="CURRENT_TIMESTAMP"}, nullable=true)
+     */
+    private $date;
+    public function getDate()
+    {
+        return $this->date;
+    }
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+}
