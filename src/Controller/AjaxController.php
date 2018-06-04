6141f6c7d653080ed1cd778593e2da067a4e394c
Gotz <sgotsulyak@usetech.ru>
2018-06-04 20:15:35 +0300

    Ratings Works! More code to come

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Entity\Likes;
+use App\Entity\Rating;
 
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
@@ -25,7 +26,7 @@ class AjaxController extends Controller
      * @Route("/ajax/like", name="ajax_like")
      * @Method({"POST"})
      */
-    public function more(UserInterface $user = null, Request $request)
+    public function like(UserInterface $user = null, Request $request)
     {
         if (!$request->isXMLHttpRequest())
             throw new BadRequestHttpException("[ERR] Only AJAX requests are allowed!");
@@ -101,6 +102,72 @@ class AjaxController extends Controller
 
         return $response;
 */
+    }
+
+    /**
+     * @Route("/ajax/rating", name="ajax_rating")
+     * @Method({"POST"})
+     */
+    public function rating(UserInterface $user = null, Request $request)
+    {
+        if (!$request->isXMLHttpRequest())
+            throw new BadRequestHttpException("[ERR] Only AJAX requests are allowed!");
+
+        if (!$user)
+            throw new AccessDeniedHttpException("[ERR] You are not logged in!");
+
+        $user = $this->getUser();
+        $user_id = $user->getId();
+
+        $allowedTypes = [ 'course', 'lecture' ];
+
+        $content = $request->getContent();
+        $params = json_decode($content, true);
+
+        $type = $params['type'] ;
+        if (!in_array($type, $allowedTypes))
+            throw new AccessDeniedHttpException("[ERR] Wrong content type!");
+
+        $id = intval($params['id']);
+        $rating = intval($params['rating']);
+
+        if (!$type || !$id)
+            throw new AccessDeniedHttpException("[ERR] Choose content for rating!");        
+
+        $ratingRepo = $this->getDoctrine()->getRepository(Rating::class);
+        $obj = $ratingRepo->findOneBy(['content_type' => $type, 'content_id' => $id, 'user_id' => $user_id]);
+
+        if (!$obj) {
+            $obj = new Rating();
+            $obj->setType($type);
+            $obj->setContentId($id);
+            $obj->setUserId($user_id);
+        }
+
+        $obj->setDate(new \DateTime());
+        $obj->setRating($rating);
+
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($obj);
+            $em->flush();
+
+            $code = 200;
+            $error = '';
+        }
+        catch (\Exception $e) {
+//            if (strpos($e->getMessage(), 'SQLSTATE[23000]')) {
+//                $code = 200;
+//                $error = '';
+//            } else {
+                $code = 500;
+                $error = $e->getMessage();
+//            }
+        }
+
+        $response = new JsonResponse([ 'code' => $code, 'error' => $error, 'type' => $type, 'id' => $id, 'rating' => $rating ]);
+        return $response;
+
     }
 
 }
--- src/Repository/RatingRepository.php
+++ src/Repository/RatingRepository.php
@@ -13,7 +13,7 @@ class RatingRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
-        parent::__construct($registry, Likes::class);
+        parent::__construct($registry, Rating::class);
     }
 
     // Get rating for one Entry
@@ -35,41 +35,5 @@ class RatingRepository extends ServiceEntityRepository
 
         return intval($rating);
     }
-
-
-/*
-    public function getLikes($content_type, $content_id)
-    {
-        $conn = $this->getEntityManager()->getConnection();
-        $likes = $conn->fetchColumn(
-            'SELECT SUM(count) FROM likes WHERE content_type = ? AND content_id = ? ',
-            [$content_type, $content_id],
-            0
-        );
-
-        return intval($likes);
-    }
-*/
-
-/*
-    public function getRating($type, $id, $user)
-    {
-        if ($user === null)
-            return 0;
-
-        if ($user instanceof UserInterface)
-            $user = $user->getId();
-
-        $conn = $this->getEntityManager()->getConnection();
-
-        $like = $conn->fetchColumn(
-            'SELECT status FROM likes WHERE content_type = ? AND content_id = ? AND user_id = ?',
-            [$type, $id, $user],
-            0
-        );
-
-        return intval($like);
-    }
-*/
 
 }
